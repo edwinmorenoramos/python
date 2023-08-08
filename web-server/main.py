d6fc0a7717b6889fe1d5bfbad9936be28534da4c
Edwin Moreno <edwinmr2@gmail.com>
2023-08-07 21:39:56 -0300

    webserver

---
+++ web-server/main.py
@@ -0,0 +1,12 @@
+import random
+
+
+import store
+def run():
+   store.get_categories()
+
+if __name__ == '__main.py__':
+   run()
+   
+   
+   
---
+++ web-server/store.py
@@ -0,0 +1,6 @@
+import requests 
+def get_categories():
+    r = requests.get ('http://api.escuelajs.co/api/v1/categories')
+    print (r.status_code)
+    print(r.text)
+    
